457ecd780ea524e14eba9682f98a54d4b060d8ca
NikiKolchev <madnike77@gmail.com>
2017-07-10 23:01:54 +0300

    Added controllers and views. Configured the 'Security class'

--- src/main/java/com/todo/configs/SecurityConfig.java
+++ src/main/java/com/todo/configs/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,16 +22,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .authorizeRequests()
+                    .antMatchers("/css/**", "/js/**").permitAll()
+                    .antMatchers("/register").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("login")
+                    .defaultSuccessUrl("/", true)
+                    .failureForwardUrl("/login?error")
+                    .permitAll()
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .deleteCookies("remember-me")
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+                .and()
+                .rememberMe();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         return super.userDetailsService();
     }
+
 }
